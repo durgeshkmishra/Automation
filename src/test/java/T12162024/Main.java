27365b3ef5cd57a0dabf4a84e74c3379e1f53f05
durgesh <durgeshkumar.mishra@gmail.com>
2024-12-18 07:08:16 -0500

    latest push

---
+++ src/test/java/T12162024/Main.java
@@ -0,0 +1,5 @@
+package T12162024;
+
+public class Main {
+    int x = 10;
+}
---
+++ src/test/java/T12162024/Second.java
@@ -0,0 +1,12 @@
+package T12162024;
+
+    public class Second{
+        public static void main(String[] args) {
+            Main myObj = new Main();
+            //myObj.x = 25;
+            System.out.println(myObj.x);
+        }
+    }
+
+
+
